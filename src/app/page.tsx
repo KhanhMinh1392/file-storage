609532ee700872c3a9b11491f911d7535ef5de01
Khanh Minh <khanhminh@MacBook-Pro-cua-Khanh.local>
2024-09-13 22:07:26 +0700

    style: reponsive section in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import { Button } from '@nextui-org/button';
 export default function Home() {
   return (
     <main className="px-6">
-      <section className="mx-auto grid h-full grid-cols-1 place-items-center md:min-h-[720px] md:max-w-7xl md:grid-cols-2">
+      <section className="mx-auto grid h-full grid-cols-1 place-items-center gap-3 max-sm:my-6 md:min-h-[720px] md:max-w-7xl md:grid-cols-2">
         <div className="max-w-xl">
-          <h1 className="text-6xl font-semibold">Easy and secure access to your content</h1>
-          <h6 className="mt-6 text-xl text-gray-500">
+          <h1 className="text-5xl font-semibold md:text-6xl">Easy and secure access to your content</h1>
+          <h6 className="mt-6 text-lg text-gray-500 md:text-xl">
             Store, share, and collaborate on files and folders from your mobile device, tablet, or computer
           </h6>
-          <div className="mt-10 flex items-center gap-6">
+          <div className="mt-6 flex items-center gap-6 max-sm:justify-center md:mt-10">
             <Button color="primary" size="lg">
               Try drive for work
             </Button>
