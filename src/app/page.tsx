8f6980acd76ccf9129baa77e937cac2b86506cdf
Khanh Minh <khanhminh@MacBook-Pro-cua-Khanh.local>
2024-09-13 21:36:56 +0700

    style: divide layout of first section in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ import { Button } from '@nextui-org/button';
 export default function Home() {
   return (
     <main className="px-6">
-      <section className="mx-auto grid grid-cols-1 place-items-center py-20 md:max-w-7xl md:grid-cols-2">
-        <div className="max-w-2xl">
+      <section className="mx-auto grid h-full grid-cols-1 place-items-center md:min-h-[720px] md:max-w-7xl md:grid-cols-2">
+        <div className="max-w-xl">
           <h1 className="text-6xl font-semibold">Easy and secure access to your content</h1>
           <h6 className="mt-6 text-xl text-gray-500">
             Store, share, and collaborate on files and folders from your mobile device, tablet, or computer
@@ -18,7 +18,7 @@ export default function Home() {
             <Button size="lg">Go to drive</Button>
           </div>
         </div>
-        <Image src={Thumbnail} alt="thumbnail" className="h-full w-auto object-cover" priority />
+        <Image src={Thumbnail} alt="thumbnail" className="w-auto min-w-full object-cover" priority />
       </section>
     </main>
   );
