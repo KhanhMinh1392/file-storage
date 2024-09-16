02a783f45d87aacc6bb6155fdf32cc053fe75604
Khanh Minh <khanhminh@MacbookPro.lan>
2024-09-16 23:14:31 +0700

    feat: create logout feature

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,8 +1,28 @@
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, User } from '@nextui-org/react';
-import { ThemeSwitcher } from '../theme-switcher';
+'use client';
+import {
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  User,
+} from '@nextui-org/react';
 import Icon from '../icon';
+import { ThemeSwitcher } from '../theme-switcher';
+import { setCookie } from '@/lib/cookies';
+import { useRouter } from 'next/navigation';
 
 export default function Header() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    setCookie('accessToken', '', 0);
+    router.refresh();
+  };
+
   return (
     <Navbar maxWidth="full" shouldHideOnScroll>
       <NavbarBrand>
@@ -14,14 +34,24 @@ export default function Header() {
           <ThemeSwitcher />
         </NavbarItem>
         <NavbarItem>
-          <User
-            name="Dao Khanh Minh"
-            description="Software Engineer"
-            avatarProps={{
-              size: 'sm',
-              src: 'https://i.pravatar.cc/150?u=a04258114e29026702d',
-            }}
-          />
+          <Dropdown placement="bottom-end">
+            <DropdownTrigger>
+              <User
+                name="Dao Khanh Minh"
+                description="Software Engineer"
+                avatarProps={{
+                  size: 'sm',
+                  src: 'https://i.pravatar.cc/150?u=a04258114e29026702d',
+                }}
+              />
+            </DropdownTrigger>
+            <DropdownMenu aria-label="Profile Actions" variant="flat" className="w-36">
+              <DropdownItem key="account">My Account</DropdownItem>
+              <DropdownItem key="logout" onClick={handleLogout}>
+                Log Out
+              </DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
         </NavbarItem>
       </NavbarContent>
     </Navbar>
